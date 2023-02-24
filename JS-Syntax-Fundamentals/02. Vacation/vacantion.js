a9809ed1689e2ce50a947afd8e224aa7dce2f971
Angel Raychev <ily_g@abv.bg>
2023-02-24 13:41:25 +0200

    task 2 js created

--- JS-Syntax-Fundamentals/02. Vacation/vacantion.js
+++ JS-Syntax-Fundamentals/02. Vacation/vacantion.js
@@ -1,7 +1,79 @@
 function totalPrice(count, type, day) {
   let price = 0;
-  let calcPrice = count * price;
+  let calcPrice = 0;
 
-  switch(day):
-  case'
+  switch (day) {
+    case "Friday":
+      if (type === "Students") {
+        price = 8.45;
+        calcPrice = count * price;
+        if (count >= 30) {
+          calcPrice *= 0.85;
+        }
+      } else if (type === "Business") {
+        price = 10.9;
+
+        if (count >= 100) {
+          count -= 10;
+        }
+        calcPrice = count * price;
+      } else if (type === "Regular") {
+        price = 15;
+        calcPrice = count * price;
+        if (count >= 10 && count <= 20) {
+          calcPrice *= 0.95;
+        }
+      }
+      break;
+    case "Saturday":
+      if (type === "Students") {
+        price = 9.8;
+        calcPrice = count * price;
+        if (count >= 30) {
+          calcPrice *= 0.85;
+        }
+      } else if (type === "Business") {
+        price = 15.6;
+
+        if (count >= 100) {
+          count -= 10;
+        }
+        calcPrice = count * price;
+      } else if (type === "Regular") {
+        price = 20;
+        calcPrice = count * price;
+        if (count >= 10 && count <= 20) {
+          calcPrice *= 0.95;
+        }
+      }
+      break;
+    case "Sunday":
+      if (type === "Students") {
+        if (type === "Students") {
+          price = 10.46;
+          calcPrice = count * price;
+          if (count >= 30) {
+            calcPrice *= 0.85;
+          }
+        } else if (type === "Business") {
+          price = 16;
+
+          if (count >= 100) {
+            count -= 10;
+          }
+          calcPrice = count * price;
+        } else if (type === "Regular") {
+          price = 22.5;
+          calcPrice = count * price;
+          if (count >= 10 && count <= 20) {
+            calcPrice *= 0.95;
+          }
+        }
+        break;
+      }
+  }
+
+  console.log(`Total price: ${calcPrice.toFixed(2)}`);
 }
+
+totalPrice(30, "Students", "Sunday");
