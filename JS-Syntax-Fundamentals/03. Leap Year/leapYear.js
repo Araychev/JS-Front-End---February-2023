f2b505d4587da21a6ffe67f06eda280ddcee7ec8
Angel Raychev <ily_g@abv.bg>
2023-02-24 17:07:43 +0200

    Task 3 added

---
+++ JS-Syntax-Fundamentals/03. Leap Year/leapYear.js
@@ -0,0 +1,10 @@
+function checkLeapYear(year) {
+  //three conditions to find out the leap year
+  if ((0 == year % 4 && 0 != year % 100) || 0 == year % 400) {
+    console.log("yes");
+  } else {
+    console.log("no");
+  }
+}
+
+checkLeapYear(4);
