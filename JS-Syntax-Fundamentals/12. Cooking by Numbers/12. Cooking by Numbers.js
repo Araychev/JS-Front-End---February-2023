b8a5622fa9e7d51bee357ecbd6d451084b5f1f78
Angel Raychev <angelraychev@gmail.com>
2023-02-25 12:13:20 +0200

    task 12 js created

---
+++ JS-Syntax-Fundamentals/12. Cooking by Numbers/12. Cooking by Numbers.js
@@ -0,0 +1,104 @@
+function solve(
+  num,
+  operation1,
+  operation2,
+  operation3,
+  operation4,
+  operation5
+) {
+  switch (operation1) {
+    case "chop":
+      num /= 2;
+      break;
+    case "dice":
+      num = Math.sqrt(num);
+      break;
+    case "spice":
+      num += 1;
+      break;
+    case "bake":
+      num *= 3;
+      break;
+    case "fillet":
+      num *= 0.8;
+      break;
+  }
+
+  console.log(num);
+  switch (operation2) {
+    case "chop":
+      num /= 2;
+      break;
+    case "dice":
+      num = Math.sqrt(num);
+      break;
+    case "spice":
+      num += 1;
+      break;
+    case "bake":
+      num *= 3;
+      break;
+    case "fillet":
+      num *= 0.8;
+      break;
+  }
+
+  console.log(num);
+  switch (operation3) {
+    case "chop":
+      num /= 2;
+      break;
+    case "dice":
+      num = Math.sqrt(num);
+      break;
+    case "spice":
+      num += 1;
+      break;
+    case "bake":
+      num *= 3;
+      break;
+    case "fillet":
+      num *= 0.8;
+      break;
+  }
+
+  console.log(num);
+  switch (operation4) {
+    case "chop":
+      num /= 2;
+      break;
+    case "dice":
+      num = Math.sqrt(num);
+      break;
+    case "spice":
+      num += 1;
+      break;
+    case "bake":
+      num *= 3;
+      break;
+    case "fillet":
+      num *= 0.8;
+      break;
+  }
+
+  console.log(num);
+  switch (operation5) {
+    case "chop":
+      num /= 2;
+      break;
+    case "dice":
+      num = Math.sqrt(num);
+      break;
+    case "spice":
+      num += 1;
+      break;
+    case "bake":
+      num *= 3;
+      break;
+    case "fillet":
+      num *= 0.8;
+      break;
+  }
+
+  console.log(num);
+}
