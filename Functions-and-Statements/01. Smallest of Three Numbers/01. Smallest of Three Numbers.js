ef051499ccbe68b7a027628a61bfbf91c36a13f7
Angel Raychev <ily_g@abv.bg>
2023-03-01 14:05:01 +0200

    task 1 js function created

---
+++ Functions-and-Statements/01. Smallest of Three Numbers/01. Smallest of Three Numbers.js
@@ -0,0 +1,13 @@
+function smalestNumber(a,b,c){
+   if(c<a && c<b){
+    console.log(c)
+   }else if(a<b &&a<c){
+    console.log(a)
+   }else{
+    console.log(b)
+   }
+}
+
+smalestNumber(600,
+    342,
+    123)
