35ebf189a85ee2254ca1ff47ce43d509b62137cc
Angel Raychev <angelraychev@gmail.com>
2023-04-06 21:25:29 +0300

    task 2 js function created

--- HTTP-and-REST/02.Bus-Schedule/app.js
+++ HTTP-and-REST/02.Bus-Schedule/app.js
@@ -1,16 +1,44 @@
 function solve() {
-    function depart() {
-        // TODO: 
-    }
+  const BASE_URL = "http://localhost:3030/jsonstore/bus/schedule/";
+  const depButton = document.getElementById("depart");
+  const arriveButton = document.getElementById("arrive");
+  const infoField = document.querySelector(".info");
+  let stopName = "depot";
 
-    async function arrive() {
-        // TODO:
-    }
+  function depart() {
+    fetch(`${BASE_URL}${stopName}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const { name, next } = data;
+        console.log(`${name} ->${next}`);
+        infoField.textContent = `Next stop ${name}`;
+        depButton.setAttribute("disabled", true);
+        arriveButton.removeAttribute("disabled");
+      })
+      .catch((error) => {
+        infoField.textContent = "Error";
+      });
+  }
 
-    return {
-        depart,
-        arrive
-    };
+  function arrive() {
+    fetch(`${BASE_URL}${stopName}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const { name, next } = data;
+        infoField.textContent = `Arriving at ${name}`;
+        arriveButton.setAttribute("disabled", true);
+        depButton.removeAttribute("disabled");
+        stopName = next;
+      })
+      .catch((error) => {
+        infoField.textContent = "Error";
+      });
+  }
+
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
