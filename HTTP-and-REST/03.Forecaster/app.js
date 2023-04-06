3017277b4ff9ddf9064b63954883a5a8a363ac10
Angel Raychev <angelraychev@gmail.com>
2023-04-06 22:17:13 +0300

    task 3 js function created

--- HTTP-and-REST/03.Forecaster/app.js
+++ HTTP-and-REST/03.Forecaster/app.js
@@ -1,5 +1,25 @@
 function attachEvents() {
-    // TODO:
+  const inputLocation = document.querySelector("#location");
+  const getWeatherButton = document.querySelector("#submit");
+  const BASE_URL = "http://localhost:3030/jsonstore/forecaster/locations";
+  const today_URL = "http://localhost:3030/jsonstore/forecaster/today/";
+  const longForecast_URL =
+    "http://localhost:3030/jsonstore/forecaster/upcoming/";
+
+  const forecast = document.getElementById("forecast");
+
+  getWeatherButton.addEventListener("click", weatherInfo);
+
+  function weatherInfo() {
+    fetch(BASE_URL)
+      .then((res) => res.json)
+      .then((data) => {
+        let searchLocation = data.find((item) => item.name === inputLocation);
+
+        console.log(searchLocation);
+      })
+      .catch(Error);
+  }
 }
 
 attachEvents();
